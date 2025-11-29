08a691d328c7a4fd5f5bcfe3f4960f82e6828727
kishornc25 <kishornc18@gmail.com>
2025-11-30 00:00:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,42 @@
 const express = require("express");
-//const cors = require("cors");
-
 const app = express();
+const mongoose = require("mongoose");
 
-// parse requests of content-type - application/json
+// Middleware
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://kishornc18_db_user:yO8hczdw9c4idWwl@cluster0.g7bf1ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-)
-.then(() => {
-  console.log("Connected to MongoDB Atlas!");
-})
-.catch(err => {
-  console.log("Cannot connect to MongoDB Atlas!", err);
-  process.exit();
-});
-// simple route
+// MongoDB Atlas Connection
+mongoose
+  .connect(
+    "mongodb+srv://kishornc18_db_user:yO8hczdw9c4idWwl@cluster0.g7bf1ut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to MongoDB Atlas!", err);
+    process.exit();
+  });
+
+// Default route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
+// Test route
+app.get("/test", (req, res) => {
+  res.json({ status: "OK", message: "Test route working" });
+});
+
+// Tutorial API routes
 require("./app/routes/tutorial.routes")(app);
 
-// set port, listen for requests
+// Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
